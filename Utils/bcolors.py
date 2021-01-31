d64fef154ce8d4e2dddb8ee77bba485b34384f37
Wedyarit <kitchenman4@yandex.ru>
2021-01-31 15:46:04 +0600

    BColors edits

--- Utils/bcolors.py
+++ Utils/bcolors.py
@@ -2,14 +2,10 @@ from options import Options
 
 class BColors:
     MAGENTA = '\033[95m'
-    BLUE = '\033[94m'
-    CYAN = '\033[96m'
     GREEN = '\033[92m'
-    YELLOW = '\033[93m'
     RED = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 def mark_to_colored(mark):
     if Options.EXCELLENT_MARK > mark > 0:
